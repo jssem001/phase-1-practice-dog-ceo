96186536f5bf5db8ca81a7cc2f6c45bc51ed8769
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-04-07 15:31:40 +0300

    append child

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+document.addEventListener('DOMContentLoaded', dogPic)
+
+function dogPic(){
+    
+  fetch(imgUrl)
+    .then(res=> res.json())
+    .then(results => {
+      results.message.forEach(image => addImage(image))
+    });
+}
+
+function addImage(dogPicUrl) {
+    let container = document.getElementById('dog-image-container');
+    let newImageEl = document.createElement('img');
+    newImageEl.src = dogPicUrl;
+    container.appendChild(newImageEl);
+  }
